a4583882f98105cc07b694c9070ccda99fa1e477
abada21 <badatunde58@gmail.com>
2022-06-25 14:59:26 +0100

    done

--- calculator.js
+++ calculator.js
@@ -1,18 +1,18 @@
 let fNumber = prompt("Enter first number");
 let operator = prompt("Choose your preferred operator(+_-_*_/)");
 let sNumber = prompt("Enter second number");
-let result = (`${fNumber} ${operator} ${sNumber}`);
+let result 
 
-if ( operator = "+"){
-    result = fNumber + sNumber;
+if ( operator == "+"){
+    result = parseInt(fNumber) + parseInt(sNumber);
 }
-if ( operator = "-"){
-    result = fNumber - sNumber;
+if ( operator == "-"){
+    result = parseInt(fNumber) - parseInt (sNumber);
 }
-if ( operator = "*"){
-    result = fNumber * sNumber;
+if ( operator == "*"){
+    result = parseInt(fNumber) * parseInt(sNumber);
 }
-if ( operator = "/"){
-    result = fNumber / sNumber;
+if ( operator == "/"){
+    result = parseInt(fNumber) / parseInt(sNumber);
 }
 alert(`${result}`);
